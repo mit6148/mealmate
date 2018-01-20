73c7125efbe2997dec4d293cc901d090f72ad890
cynliu98 <cynliu98@mit.edu>
2018-01-20 01:46:05 -0500

    render actual match in matches.js

--- public/js/matches.js
+++ public/js/matches.js
@@ -11,52 +11,45 @@ function main(){
 // render Matches
 function renderMatches(user) {
 
-    const matchTableDiv = document.getElementById('matchTable');
-
     if (user.matches.length) { // if the user has matches
         // create table
-        const matchTable = document.createElement('table')
-        matchTable.setAttribute('id', 't01');
+        const matchTable = document.getElementById('t01')
 
         // render matches in table
         for (let i=0; i<user.matches.length; i++) {
-            matchTable.appendChild(renderMatch(user.matches[i]));
+            // console.log("Here's the userid of matches[i]: " + user.matches[i].userid);
+            get('/api/user', { '_id': user.matches[i].userid }, function (user) {
+                const tabrow = document.createElement('tr');
+
+                const matchPic = document.createElement('th');
+                const profileImage = document.createElement('img');
+                profileImage.src = 'https://graph.facebook.com/'+user.fbid+'/picture?type=large';
+                matchPic.appendChild(profileImage);
+
+                const matchName = document.createElement('th');
+                matchName.innerHTML = user.name;
+                const matchDate = document.createElement('th');
+                // tbh this date formatting is hacky 
+                matchDate.innerHTML = user.matches[i].date.substring(0,10);
+                const matchTime = document.createElement('th');
+                matchTime.innerHTML = user.matches[i].times[0]; // just choose the first available time for now
+                const matchHall = document.createElement('th');
+                matchHall.innerHTML = user.matches[i].halls[0]; // choose first hall
+
+                tabrow.appendChild(matchPic);
+                tabrow.appendChild(matchName);
+                tabrow.appendChild(matchDate);
+                tabrow.appendChild(matchTime);
+                tabrow.appendChild(matchHall);
+                matchTable.appendChild(tabrow);
+            });
         }
-        matchTableDiv.appendChild(matchTable);
+
     } else { // don't display a table if no matches
         const noMatch = document.createElement('p');
         noMatch.innerHTML = 'No matches yet! Check back soon.';
         matchTableDiv.appendChild(noMatch);
     }
-}
-
-// render a single match
-// profile picture, name, time, and dining hall
-function renderMatch(match) {
-    const tabrow = document.createElement('tr');
-
-    // step 1: hardcode
-    const matchPic = document.createElement('th');
-    matchPic.innerHTML = ":D";
-    const matchName = document.createElement('th');
-    matchName.innerHTML = ":D";
-    const matchDate = document.createElement('th');
-    matchDate.innerHTML = "Mon Jan 1 2000";
-    const matchTime = document.createElement('th');
-    matchTime.innerHTML = "5:30 pm";
-    const matchHall = document.createElement('th');
-    matchHall.innerHTML = "The best dining hall";
-
-    tabrow.appendChild(matchPic);
-    tabrow.appendChild(matchName);
-    tabrow.appendChild(matchDate);
-    tabrow.appendChild(matchTime);
-    tabrow.appendChild(matchHall);
-
-    return tabrow
-    /*get('/api/user', { '_id': match.userid }, function (user) {
-
-    });*/
 }
 
 main();
