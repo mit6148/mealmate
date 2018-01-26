bab153531e4a433aa088081de8062c1e760d452b
Nanxi Liu <nanxi@mit.edu>
2018-01-25 20:26:20 -0500

    Made navbar item bold if you're on the page woo

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,18 @@
-function newNavbarItem(text, url, currentPath) {
+function newNavbarItem(text, url, currentPath, user) {
 	const itemBox = document.createElement('li');
 	const itemLink = document.createElement('a');
 	itemLink.innerHTML = text;
 	itemLink.href = url;
+    console.log("url: " + url);
+    console.log("currentPath: " + currentPath);
 	if (url == currentPath) {
 		itemLink.className = "active";
 	}
+    if (user != undefined) {
+        if (url == (currentPath + "?" + user._id)) {
+            itemLink.className = "active";
+        }
+    }
 	itemBox.appendChild(itemLink);
 
 	return itemBox;
@@ -24,12 +31,12 @@ function renderNavbar(user){
     navbarObjects.appendChild(newNavbarItem('ABOUT', '/about', currentPath));
     
 	if (user._id !== undefined) {
-		navbarObjects.appendChild(newNavbarItem('PROFILE', '/u/profile?'+user._id, currentPath)); 
-        navbarObjects.appendChild(newNavbarItem('FIND', '/u/findMealmate?'+user._id, currentPath));
-        navbarObjects.appendChild(newNavbarItem('MATCHES', '/u/matches?'+user._id, currentPath));
-		navbarObjects.appendChild(newNavbarItem('LOGOUT', '/logout', currentPath));
+		navbarObjects.appendChild(newNavbarItem('PROFILE', '/u/profile?'+user._id, currentPath, user)); 
+        navbarObjects.appendChild(newNavbarItem('FIND', '/u/findMealmate?'+user._id, currentPath, user));
+        navbarObjects.appendChild(newNavbarItem('MATCHES', '/u/matches?'+user._id, currentPath, user));
+		navbarObjects.appendChild(newNavbarItem('LOGOUT', '/logout', currentPath, user));
 	} else {
-		navbarObjects.appendChild(newNavbarItem('LOGIN', 'auth/facebook', currentPath)); // add this when FB authentication happens
+		navbarObjects.appendChild(newNavbarItem('LOGIN', 'auth/facebook', currentPath, undefined)); // add this when FB authentication happens
 	}
 	
 	navbarDiv.appendChild(navbarObjects);
