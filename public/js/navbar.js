d8f747341e16a4d21c376ba2a03732c8aafa060c
Sabrina Liu <sabrinaliu@dhcp-18-189-8-63.dyn.mit.edu>
2018-01-25 20:24:42 -0500

    Added dropdown menus in navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -11,22 +11,57 @@ function newNavbarItem(text, url, currentPath) {
 	return itemBox;
 }
 
+function newDropdown(text, links, linkLabels, currentPath){
+	const dropdownBox = document.createElement('div');
+	dropdownBox.className = "dropdown";
+
+	const dropdownBtn = document.createElement('button');
+	dropdownBox.appendChild(dropdownBtn);
+	dropdownBtn.className = "dropbtn";
+	dropdownBtn.innerHTML =  text + "<i class='fa fa-caret-down'></i>";
+	dropdownBtn.addEventListener('click', function(){
+		document.location.href = links[0];
+	})
+
+	const dropdowncontent = document.createElement('div');
+	dropdownBox.appendChild(dropdowncontent);
+	dropdowncontent.className = "dropdown-content";
+
+	for (let i = 0; i < links.length; i++){
+		//dropdowncontent.appendChild(newNavbarItem(linkLabels[i], links[i], currentPath));
+		const dropdownLink = document.createElement('a');
+		dropdowncontent.appendChild(dropdownLink);
+		dropdownLink.href = links[i];
+		dropdownLink.innerHTML = linkLabels[i];
+		dropdownLink.addEventListener("click", function(){
+			document.location.href = links[i];
+		});
+		/*
+		if (links[i] === currentPath){
+			dropdownLink.className = "active";
+		}
+		*/
+	}
+	console.log(dropdowncontent);
+
+	return dropdownBox;
+
+}
+
 function renderNavbar(user){
 	const navbarDiv = document.getElementById('myNavbar');
 	const navbarObjects = document.createElement('ul');
 	navbarObjects.className = "nav navbar-nav navbar-right";
 
 	const currentPath = window.location.pathname; // get the current page
-    const aboutHeader = newNavbarItem('ABOUT', '/about', currentPath); 
-    const matchesHeader = newNavbarItem('MATCHES', '/u/matches?'+user._id, currentPath); 
 	
 	navbarObjects.appendChild(newNavbarItem('HOME', '/', currentPath));
     navbarObjects.appendChild(newNavbarItem('ABOUT', '/about', currentPath));
     
 	if (user._id !== undefined) {
-		navbarObjects.appendChild(newNavbarItem('PROFILE', '/u/profile?'+user._id, currentPath)); 
-        navbarObjects.appendChild(newNavbarItem('FIND', '/u/findMealmate?'+user._id, currentPath));
-        navbarObjects.appendChild(newNavbarItem('MATCHES', '/u/matches?'+user._id, currentPath));
+		//navbarObjects.innerHTML = '<div class="dropdown"> <button class="dropbtn">Dropdown <i class="fa fa-caret-down"></i> </button> <div class="dropdown-content">  <a href="#">Link 1</a>  <a href="#">Link 2</a> <a href="#">Link 3</a> </div </div> ';
+		navbarObjects.appendChild(newDropdown('PROFILE', ['/u/profile?'+user._id, '/u/edit?'+user._id], ['VIEW', 'EDIT'], currentPath));
+        navbarObjects.appendChild(newDropdown('MATCHES', ['/u/matches?'+user._id, '/u/findMealmate?'+user._id], ['CURRENT', 'FIND'], currentPath));
 		navbarObjects.appendChild(newNavbarItem('LOGOUT', '/logout', currentPath));
 	} else {
 		navbarObjects.appendChild(newNavbarItem('LOGIN', 'auth/facebook', currentPath)); // add this when FB authentication happens
