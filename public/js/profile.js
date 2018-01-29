675fb76ad0712edc787caf046994c208b4e2caa4
Nanxi Liu <nanxi@mit.edu>
2018-01-29 17:47:45 -0500

    Made button display = none if not their profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -35,7 +35,7 @@ function main() {
         } else if (user._id != profileId) {
             // you're viewing someone else's profile
             const editButton = document.getElementById('editProfile');
-            editButton.style.visibility = "none";
+            editButton.style.display = "none";
         } else {
             loadEditButton(user);
         }
